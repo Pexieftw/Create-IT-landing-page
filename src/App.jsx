d6531a009e8736a9129f99360786bdd78e8486dd
Pexieftw <idir0204@gmail.com>
2023-02-23 21:38:02 +0100

    Finished Description & Added Scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import Navbar from './Navbar'
 import Home from './Home'
+
 function App() {
   const [count, setCount] = useState(0)
 
+  function handler(e){
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({behavior: 'smooth'});
+  }
+
+  useEffect(() => {
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+      anchor.addEventListener('click', handler);
+    })
+    return () => {
+      document.querySelectorAll('a[href^="#"]').forEach(anchor => { 
+        anchor.removeEventListener('click', handler);
+      })
+    } 
+  })
+  
   return (
     <div className="App">
       <Navbar />
